3e201a82d9f458bef9458a9998c061a1124e4b01
Kamal Debnath <76668054+kamaldebnath@users.noreply.github.com>
2022-09-14 20:15:55 +0530

    Update CustomString.java

--- Week2_Homework 2 : Custom String/CustomString.java
+++ Week2_Homework 2 : Custom String/CustomString.java
@@ -1,80 +1,159 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.*;  
-public class CustomString{
-	String myString;
-	boolean isSet;
 
+public class CustomString{
+
+	//instance variables
+	
+	String myString;
+	
+	boolean isSet;
+	
+	//constructor
+	
 	public CustomString() {
 		// TODO Implement constructor
-        this.myString = null;
-        this.isSet = false;
+		this.myString = null;
+		this.isSet = false;
 	}
-
+	
+	// methods 
+	
 	public String getString() {
 		// TODO Implement method
-        if (this.myString != null) {
-            return myString;
-        }
-        else
-            return null;
+		if(this.myString == null || this.isSet == false)
+			return null;
+		else
+			return this.myString;
 	}
-
+	
 	public void setString(String string) {
 		// TODO Implement method
-        if (this.myString == null) {
-            this.myString = string;
-            this.isSet = true;
-        } else {
-            this.isSet = false;
-        }
+		if(string == null)
+			this.isSet = false;
+		else
+		{
+			this.myString = string;
+			this.isSet = true;
+		}
 	}
-
-   
-	public String remove(String arg) {
-		// TODO Implement method
-		if (this.myString==null || this.myString == "") {
-            return this.myString;
-        }
-        if (myString != null) {
-            this.myString = myString.replaceAll(arg,"");
-            System.out.println(myString);
-        }
 		
-		return null;
+	public String remove(String arg) {
+		 // TODO Implement method
+		String newString = "";
+		
+		if(this.myString == null || this.myString.isEmpty() || this.isSet == false)
+			return newString;
+		else
+		{
+			newString = this.myString;
+			
+			String temp;
+			
+			for(int i=0; i<arg.length(); ++i)
+			 {
+				temp = "";
+						
+				if(Character.isLetter(arg.charAt(i)))
+				{
+					for(int j=0; j<newString.length(); ++j)
+					{
+						if(newString.charAt(j) != arg.charAt(i))
+							temp = temp + newString.charAt(j);
+					}	
+					
+					newString = temp;
+				}
+				
+			 }
+				 
+			 return newString;
+		}
 	}
-	
 	
 	public String reverse(String arg) {
-		// TODO Implement method 
-		char[] charArray = myString.toCharArray();
-            for(int i=0; i < charArray.length; i++){
-                if( Character.isUpperCase(charArray[i]) ){
-                    charArray[i] = Character.toLowerCase( charArray[i] );
-                }
-                else if(Character.isLowerCase(charArray[i]) ){
-                    charArray[i] = Character.toUpperCase( charArray[i] );
-                }    
-            }
-        myString = new String(charArray);
-		return null;
+		 // TODO Implement method 
+		String newString = "";
+		
+		if(this.myString == null || this.myString.isEmpty() || this.isSet == false)
+			return newString;
+		else
+		{
+			newString = this.myString;
+			
+			String temp;
+			
+			for(int i=0; i<arg.length(); ++i)
+			 {
+				temp = "";
+						
+				if(Character.isLetter(arg.charAt(i)))
+				{
+					for(int j=0; j<newString.length(); ++j)
+					{
+						char c1 = newString.charAt(j);
+						char c2 = arg.charAt(i);
+						
+						if(Character.toLowerCase(c1) == Character.toLowerCase(c2))
+						{
+							if(Character.isLowerCase(c1))
+								temp = temp + Character.toUpperCase(c1);
+							else
+								temp = temp + Character.toLowerCase(c1);
+						}
+						else
+							temp = temp + c1;
+					}	
+					
+					newString = temp;
+				}
+				
+			 }
+				 
+			 return newString;
+		}
 	}
-
-    
+		   
 	public String filterLetters(char n, boolean more) {
-		// TODO Implement method
-	String newStr = "";
-        for (int i = 0; i < this.myString.length(); i++) {
-            if (myString.charAt(i) >= n && more) {
-                newStr = newStr + myString.charAt(i);
-            } else if (myString.charAt(i) <= n && !more) {
-                newStr = newStr + myString.charAt(i);
-            }
-
-            if(!Character.isLetter(myString.charAt(i))) {
-                newStr = newStr + myString.charAt(i);
-            }
-        }
-        return newStr;
-
+		 // TODO Implement method
+		String newString = "";
+		
+		if(this.myString == null || this.myString.isEmpty() || this.isSet == false || !Character.isLetter(n))
+			return newString;
+		else
+		{			
+			if(more)
+			{
+				for(int i=0; i<this.myString.length(); ++i)
+				{
+					char c = myString.charAt(i);
+					
+					if(Character.isLetter(c))
+					{
+						if((c >= 'a' && c < Character.toLowerCase(n)) || (c >= 'A' && c < Character.toUpperCase(n)))
+							newString = newString + c;						
+					}
+					else
+						newString = newString + c;
+				}
+			}
+			else
+			{
+				for(int i=0; i<this.myString.length(); ++i)
+				{
+					char c = myString.charAt(i);
+					
+					if(Character.isLetter(c))
+					{
+						if((c > Character.toLowerCase(n) && c <= 'z') || (c > Character.toUpperCase(n) && c <= 'Z'))
+							newString = newString + c;
+					}
+					else
+						newString = newString + c;
+				}
+			}
+			
+			return newString;
+		}
 	}
 }
